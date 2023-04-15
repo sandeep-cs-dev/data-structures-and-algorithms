440301ee1e898c79ff478fe84e0fa4fd2b96c835
sandeep-cs-dev <sandeeppatelsgsits@gmail.com>
2023-04-16 01:39:14 +0200

    jumpgame

--- java/src/leetcode/JumpGame.java
+++ java/src/leetcode/JumpGame.java
@@ -7,14 +7,11 @@ class JumpGame {
         if (nums.length == 1) {
             return true;
         }
-        if (nums[0] == 0) {
-            return false;
-        }
         for (int i = 0; i < nums.length - 1; i++) {
+            maxReach = Math.max(maxReach, nums[i] + i);
             if (nums[i] == 0 && maxReach <= i) {
                 return false;
             }
-            maxReach = Math.max(maxReach, nums[i] + i);
             if (maxReach >= nums.length - 1) {
                 return true;
             }
